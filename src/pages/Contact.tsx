57c757fc8c16950fc4e268d9602250010e3344ae
Aldo Wiranata <148690939+alwiranata@users.noreply.github.com>
2025-01-27 21:51:38 +0700

    update: Navbar.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,8 @@
+import { FaEnvelope, FaPhoneAlt, FaMapMarkerAlt } from "react-icons/fa";
+
 const Contact = () => {
     return (
         <div className="container py-5">
-
             <div className="row align-items-center">
                 {/* Kolom Gambar */}
                 <div className="col-md-5 text-center">
@@ -16,21 +17,26 @@ const Contact = () => {
                 {/* Kolom Informasi Kontak */}
                 <div className="col-md-7">
                     <div className="contact-info ps-md-4">
-                    <h2 className="display-5 fw-bold text-success mb-3">Contact Us</h2>
-                        <p className="text-muted">For questions and suggestions about our services, please fill out the form below.</p>
-                        <p className="text-muted"> Responses to enquiries and suggestions will be processed as soon as possible on working days: Monday - Friday 9:00 - 18:00.</p>
+                        <h2 className="display-5 fw-bold text-success mb-3">Contact Us</h2>
+                        <p className="text-muted">
+                            For questions and suggestions about our services, please fill out the form below.
+                        </p>
+                        <p className="text-muted">
+                            Responses to enquiries and suggestions will be processed as soon as possible on working days: 
+                            Monday - Friday 9:00 - 18:00.
+                        </p>
                         <ul className="list-unstyled">
                             <li className="mb-3">
-                                <i className="bi bi-envelope-fill text-primary me-2"></i>
-                                <strong className="text-warning">Email :  </strong> aldowiranata16@gmail.com
+                                <FaEnvelope className="text-success me-2" />
+                                aldowiranata16@gmail.com
                             </li>
                             <li className="mb-3">
-                                <i className="bi bi-telephone-fill text-primary me-2"></i>
-                                <strong className="text-warning">Phone :</strong> +62 895 6348 89510
-                            </li> 
+                                <FaPhoneAlt className="text-success me-2" />
+                                 +62 895 6348 89510
+                            </li>
                             <li className="mb-3">
-                                <i className="bi bi-geo-alt-fill text-primary me-2"></i>
-                                <strong className="text-warning">Address :</strong> Jl. Kesuma No. 24, Pekanbaru
+                                <FaMapMarkerAlt className="text-success me-2" />
+                                 Jl. Kesuma No. 24, Pekanbaru
                             </li>
                         </ul>
                     </div>
@@ -42,15 +48,15 @@ const Contact = () => {
                 <div className="col-md-8">
                     <form>
                         <div className="mb-3">
-                            <label htmlFor="name" className="form-label fw-bold text-danger">Full Name</label>
+                            <label htmlFor="name" className="form-label fw-bold text-success">Full Name</label>
                             <input type="text" className="form-control" id="name" placeholder="Enter your name" required />
                         </div>
                         <div className="mb-3">
-                            <label htmlFor="email" className="form-label fw-bold text-danger">Email Address</label>
+                            <label htmlFor="email" className="form-label fw-bold text-success">Email Address</label>
                             <input type="email" className="form-control" id="email" placeholder="Enter your email" required />
                         </div>
                         <div className="mb-3">
-                            <label htmlFor="message" className="form-label fw-bold text-danger">Message</label>
+                            <label htmlFor="message" className="form-label fw-bold text-success">Message</label>
                             <textarea className="form-control" id="message" placeholder="Write your message" required></textarea>
                         </div>
                         <button type="submit" className="btn btn-primary w-100">Send Message</button>
--- src/pages/Navbar.tsx
+++ src/pages/Navbar.tsx
@@ -1,36 +1,77 @@
-import { Link, useLocation } from "react-router"
+import { Link, useLocation } from "react-router";
 
 const Navbar = () => {
-    const location = useLocation()
-    
+    const location = useLocation();
+
     return (
         <>
             <nav className="navbar navbar-expand-lg bg-body-tertiary bg-primary fixed-top sticky-top">
                 <div className="container-fluid">
+                    {/* Logo */}
+                    <a className="navbar-brand" href="#">
+                        <img src="/fruit.png" alt="Not File" width="30" height="24" />
+                    </a>
+
+                    {/* Button Toggle for Mobile View */}
+                    <button 
+                        className="navbar-toggler" 
+                        type="button" 
+                        data-bs-toggle="collapse" 
+                        data-bs-target="#navbarSupportedContent" 
+                        aria-controls="navbarSupportedContent" 
+                        aria-expanded="false" 
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <a className="navbar-brand" href="#">
-                                <img src="/fruit.png" alt="Not File" width="30" height="24"/>
-                            </a>
                             <li className="nav-item">
-                                <Link className={`nav-link ${location.pathname === "/" ? "text-primary" : ""}`} aria-current="page" to="/">Welcome</Link>
+                                <Link 
+                                    className={`nav-link ${location.pathname === "/" ? "text-warning fw-bold" : ""}`} 
+                                    to="/"
+                                >
+                                    Welcome
+                                </Link>
                             </li>
                             <li className="nav-item">
-                                <Link className={`nav-link ${location.pathname === "/product" ? "text-primary" : ""}`} aria-current="page" to="/product">Product</Link>
+                                <Link 
+                                    className={`nav-link ${location.pathname === "/product" ? "text-warning fw-bold" : ""}`} 
+                                    to="/product"
+                                >
+                                    Product
+                                </Link>
                             </li>
                             <li className="nav-item">
-                                <Link className={`nav-link ${location.pathname === "/about" ? "text-primary" : ""}`} aria-current="page" to="/about">About</Link>
+                                <Link 
+                                    className={`nav-link ${location.pathname === "/about" ? "text-warning fw-bold" : ""}`} 
+                                    to="/about"
+                                >
+                                    About
+                                </Link>
                             </li>
                             <li className="nav-item">
-                                <Link className={`nav-link ${location.pathname === "/contact" ? "text-primary" : ""}`} aria-current="page" to="/contact">Contact</Link>
+                                <Link 
+                                    className={`nav-link ${location.pathname === "/contact" ? "text-warning fw-bold" : ""}`} 
+                                    to="/contact"
+                                >
+                                    Contact
+                                </Link>
                             </li>
                         </ul>
 
+                        {/* Tombol Login Paling Kanan */}
+                        <ul className="navbar-nav ms-auto">
+                            <li className="nav-item">
+                                <button className="btn btn-outline-warning text-secondary-emphasis" type="submit">Login</button>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </nav>
         </>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
